d95c3b63be4292795df09fa81b90f3916bd29b31
Eezi <tomi.a.salo@outlook.com>
2023-02-25 17:42:12 +0200

    Added workoutSessionRouter to the appRouter

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,9 +1,11 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { workoutRouter } from "./workout";
+import { workoutSessionRouter } from "./workoutSession";
 
 export const appRouter = router({
   workout: workoutRouter,
+  workoutSession: workoutSessionRouter,
   auth: authRouter,
 });
 
