40e1999c7ab4ee428d37a3e0a502d81a68d051f5
Eezi <tomi.a.salo@outlook.com>
2023-11-18 18:36:45 +0200

    Added active links for phone nav links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,14 +7,106 @@ const pages = [
   {
     link: "/statistics",
     name: "Statics",
+    icon: (
+      <svg
+        className="h-5 w-5"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+      >
+        <rect
+          width="48"
+          height="160"
+          x="64"
+          y="320"
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="32"
+          rx="8"
+          ry="8"
+        />
+        <rect
+          width="48"
+          height="256"
+          x="288"
+          y="224"
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="32"
+          rx="8"
+          ry="8"
+        />
+        <rect
+          width="48"
+          height="368"
+          x="400"
+          y="112"
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="32"
+          rx="8"
+          ry="8"
+        />
+        <rect
+          width="48"
+          height="448"
+          x="176"
+          y="32"
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="32"
+          rx="8"
+          ry="8"
+        />
+      </svg>
+    ),
   },
   {
     link: "/workout-sessions",
     name: "Sessions",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+        />
+      </svg>
+    ),
   },
   {
     link: "/allworkouts",
     name: "Workouts",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+        />
+      </svg>
+    ),
   },
 ];
 
@@ -75,119 +167,30 @@ export const Navbar = () => {
 
 export const BottomNavBar = () => {
   const { data: sessionData } = useSession();
-  // Tämä pitäisi eristää omaksi componenteiksi jotta saadaan active className lisättyä kivasti
-  // className="active"
-  // const router = useRouter();
-
+  const pathname = usePathname();
   if (!sessionData) return null;
-
-  // VOisko tää layout issue johtua siitä että tätä käytetään väärässä paikassa
-  //
-  //const isDesktop = window.screen.width > 768;
-
-  //if (isDesktop) return null;
 
   return (
     <div
       data-theme="nightforest"
       className="btm-nav fixed bottom-0 p-2 pb-5 md:hidden"
     >
-      <Link href="/workout-sessions">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-          />
-        </svg>
-        <span className="btm-nav-label text-xs">Sessions</span>
-      </Link>
-      <Link href="/allworkouts">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-          />
-        </svg>
-
-        <span className="btm-nav-label text-xs">Workouts</span>
-      </Link>
-      <Link href="/statistics">
-        <svg
-          className="h-5 w-5"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 512 512"
-        >
-          <rect
-            width="48"
-            height="160"
-            x="64"
-            y="320"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="32"
-            rx="8"
-            ry="8"
-          />
-          <rect
-            width="48"
-            height="256"
-            x="288"
-            y="224"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="32"
-            rx="8"
-            ry="8"
-          />
-          <rect
-            width="48"
-            height="368"
-            x="400"
-            y="112"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="32"
-            rx="8"
-            ry="8"
-          />
-          <rect
-            width="48"
-            height="448"
-            x="176"
-            y="32"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="32"
-            rx="8"
-            ry="8"
-          />
-        </svg>
-        <span className="btm-nav-label text-xs">Statics</span>
-      </Link>
+      {pages.map(({ name, link, icon }) => {
+        const isActive = pathname === link;
+        return (
+          <Link
+            style={{
+              textDecoration: isActive ? "underline" : "none",
+              textDecorationThickness: "2px",
+              textUnderlineOffset: "3px",
+            }}
+            href={link}
+          >
+            {icon}
+            <span className="btm-nav-label text-xs">{name}</span>
+          </Link>
+        );
+      })}
     </div>
   );
 };
