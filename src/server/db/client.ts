5f6cfa81f233b4e74f437be8e9c192a23ab07239
Eezi <tomi.a.salo@outlook.com>
2024-03-22 19:44:23 +0200

    Migrated db to Turso from the Planetscale

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,18 +1,24 @@
 import { PrismaClient } from "@prisma/client";
-
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 import { env } from "../../env/server.mjs";
+
+const libsql = createClient({
+  url: env.TURSO_DATABASE_URL,
+  authToken: env.TURSO_AUTH_TOKEN
+});
+
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+const adapter = new PrismaLibSQL(libsql);
+export const prisma = global.prisma || new PrismaClient({ 
+  adapter,
+  log: env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+});
 
 if (env.NODE_ENV !== "production") {
   global.prisma = prisma;
