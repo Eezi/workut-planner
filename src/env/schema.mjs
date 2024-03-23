e3e7021743791294b84a7a40493d645a3616ffda
Eezi <tomi.a.salo@outlook.com>
2024-03-23 10:50:37 +0200

    updated env zod schema

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -8,7 +8,7 @@ import { z } from "zod";
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   TURSO_DATABASE_URL: z.string(),
-  TURSO_AUTH_TOKEN: z.string().url(),
+  TURSO_AUTH_TOKEN: z.string(),
   NODE_ENV: z.enum(["development", "test", "production"]).optional(),
   NEXTAUTH_SECRET:
     process.env.NODE_ENV === "production"
