b74d63c1565154f76986493e1382cdfb81a4b86d
Tomi Salo <tomi.salo@example.com>
2026-09-05 13:49:21 +0300

    fix: fixed datepicker closing issue

--- src/components/Datepicker.tsx
+++ src/components/Datepicker.tsx
@@ -1,43 +1,52 @@
 "use client";
-import React from "react";
 import { CalendarIcon } from "@radix-ui/react-icons";
 import { format } from "date-fns";
-
-import { cn } from "@/lib/utils";
+import React from "react";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
 } from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
 
 type TSetDate = (date: Date) => void;
 export type SetDate = TSetDate | React.Dispatch<React.SetStateAction<Date>>;
 export function DatePicker({
-  date,
-  setDate,
+	date,
+	setDate,
 }: {
-  date: Date;
-  setDate: SetDate;
+	date: Date;
+	setDate: SetDate;
 }) {
-  return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant={"outline"}
-          className={cn(
-            "w-[240px] justify-start text-left font-normal",
-            !date && "text-muted-foreground"
-          )}
-        >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, "PPP") : <span>Pick a date</span>}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0" align="start">
-        <Calendar mode="single" selected={date} onSelect={setDate as any} />
-      </PopoverContent>
-    </Popover>
-  );
+	const [open, setOpen] = React.useState(false);
+	return (
+		<Popover open={open} onOpenChange={setOpen}>
+			<PopoverTrigger asChild>
+				<Button
+					variant={"outline"}
+					className={cn(
+						"w-[240px] justify-start text-left font-normal",
+						!date && "text-muted-foreground",
+					)}
+				>
+					<CalendarIcon className="mr-2 h-4 w-4" />
+					{date ? format(date, "PPP") : <span>Pick a date</span>}
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent className="w-auto p-0" align="start">
+				<Calendar
+					mode="single"
+					selected={date}
+					onSelect={(selected) => {
+						if (selected) {
+							(setDate as TSetDate)(selected);
+							setOpen(false);
+						}
+					}}
+				/>
+			</PopoverContent>
+		</Popover>
+	);
 }
